b8a32bab673fd09d4a4e8fcf66db779c367c4cba
HenriqueRCampos <henriquetimon22@gmail.com>
2025-08-07 22:38:49 -0300

    refactor(prototype): ♻️ Modified player script
    - Variables, methods and components to implement player movement

--- Assets/Scripts/InfinityRunner/Player.cs
+++ Assets/Scripts/InfinityRunner/Player.cs
@@ -2,18 +2,40 @@ using UnityEngine;
 
 namespace InfinityRunner
 {
+    [RequireComponent(typeof(Rigidbody2D))]
     public class Player : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
+        [SerializeField]
+        private float movementSpeed;
 
+        [SerializeField]
+        private float jumpForce;
+
+        private Rigidbody2D playerRigidbody;
+        private PlayerInput playerInput;
+
+        private void Awake()
+        {
+            playerInput = GetComponent<PlayerInput>();
+            playerRigidbody = GetComponent<Rigidbody2D>();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Update()
         {
+            if(playerInput.JumpWasPressed)
+            {
+                Jump();
+            }
+        }
 
+        private void FixedUpdate()
+        {
+            playerRigidbody.linearVelocityX = movementSpeed;
+        }
+
+        private void Jump()
+        {
+            playerRigidbody.AddForceAtPosition(new Vector2(0, jumpForce), Vector2.up, ForceMode2D.Impulse);
         }
     }
 }
